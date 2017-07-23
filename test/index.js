ab3aa19c2a1590f7367b9b5603909a74d9bce32b
Beau Lynn-Miller <beaulm@gmail.com>
2017-07-23 12:29:15 -0700

    Add outline for tests

---
+++ test/index.js
@@ -0,0 +1,6 @@
+//Get email lists with different distributions of duplicates
+//Get all dedupe methods
+//For each email list
+	//For each dedupe method
+		//Make sure the output is correct
+		//Make sure the method returns in less than one second
