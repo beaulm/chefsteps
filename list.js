82325ce553af8f6ea065dde18ef587aceb63c78e
Beau Lynn-Miller <beaulm@gmail.com>
2017-07-23 12:44:20 -0700

    Outline list functions

---
+++ list.js
@@ -0,0 +1,7 @@
+//Generate a list of 50,000 unique email addresses
+
+//Push the list of email addresses into an array twice (steps)
+//Push the list of email addresses into an array, then reverse it and push it in again (pyramid)
+//Create a list of email addresses where each address appears twice in a row (double)
+//Create a list of email addresses where each address apepars twice, but in a random order (knuth)
+	//Perform a knuth shuffle on any of the previous lists
